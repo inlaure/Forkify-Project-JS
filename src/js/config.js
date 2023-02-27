8a4e4ad83cc0810326c4b0c8c1ac928a32c029b6
inlaure <laurantnvt@gmail.com>
2023-02-27 14:44:22 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = `fbf80ed5-f82b-4072-9a47-1442fc63cce6`;
+export const MODAL_CLOSE_SEC = 2.5;
